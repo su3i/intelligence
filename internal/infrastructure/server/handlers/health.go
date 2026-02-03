41288b072984b7bfc0b06c74c1066052955bb108
Folarin Raphael <chrisraphael576@gmail.com>
2026-02-04 00:06:27 +0100

    Health Check

--- internal/infrastructure/server/handlers/health.go
+++ internal/infrastructure/server/handlers/health.go
@@ -11,6 +11,9 @@ func Health(c *gin.Context) {
 	log.Print("Received health check request.")
 
 	c.JSON(http.StatusOK, gin.H{
-		"message": "Suei Intelligence is healthy!",
+		"message": "Healthy",
+		"version": "v0.0.0",
+		"copyright": "2026 SUEI INC.",
+		"uptime": "",
 	  })
 }
